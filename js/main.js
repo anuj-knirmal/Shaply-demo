e7ac01cd8ae8e432e769ac13471020e4ff7ffce4
Anuj Kumar Nirmal <anujkumarnirmal199@gmail.com>
2025-11-11 23:42:55 +0530

    initial commit shaplyDemo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 4, 
+      loop: true, 
+      margin: 20, 
+      nav: true, 
+      autoplay: true,
+      autoplayTimeout: 2000
+    });
+  });
